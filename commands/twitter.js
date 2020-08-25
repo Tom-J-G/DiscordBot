d5b19f78b31d30e601e543a189ed166cc8cba50b
Thomas <thomas.j.goodhew@gmail.com>
2020-08-25 11:56:20 +0100

    fixes

--- commands/twitter.js
+++ commands/twitter.js
@@ -9,7 +9,6 @@ module.exports = {
     name: 'twitter',
     getTweet: (T, msg) => {
         T.get('statuses/user_timeline', params, (error, tweets) => {
-            console.log(tweets);
             const tweet = tweets[0];
             const twitterMessage = `${tweet.user.name} (@${tweet.user.screen_name}) tweeted this: https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
             msg.channel.send(twitterMessage);
